7d16edb8d7df57c13bd088d489963aa8074f7609
pandikani <pandikani636@gmail.com>
2026-08-18 20:54:43 +0530

    Fix product fetching and database connection

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,12 @@ const asyncErrorHandler = require('./asyncErrorHandler');
 
 exports.isAuthenticatedUser = asyncErrorHandler(async (req, res, next) => {
 
-    const { token } = req.cookies;
+    let token = req.cookies.token;
+
+    // Check for token in Authorization header
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+    }
 
     if (!token) {
         return next(new ErrorHandler("Please Login to Access", 401))
