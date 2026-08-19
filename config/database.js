05acbe8067cd5abdc8a5b795220415248089a08a
pandikani <pandikani636@gmail.com>
2026-08-19 13:45:11 +0530

    Revert "Increase mongoose bufferTimeoutMS to prevent Vercel Serverless cold start timeouts"
    This reverts commit 791dd446b4774c6d03eb5cd6762867d22a23855c.

--- config/database.js
+++ config/database.js
@@ -2,23 +2,14 @@ const mongoose = require('mongoose');
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/flipkart";
 
 
-const connectDatabase = async () => {
-    if (mongoose.connection.readyState >= 1) {
-        console.log("Mongoose already connected");
-        return;
-    }
-    
-    try {
-        await mongoose.connect(MONGO_URI, { 
-            useNewUrlParser: true, 
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 30000, // 30 seconds
-            bufferTimeoutMS: 30000, // 30 seconds
+const connectDatabase = () => {
+    mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log("Mongoose Connected");
+        })
+        .catch((err) => {
+            console.error("MongoDB Connection Error: ", err);
         });
-        console.log("Mongoose Connected");
-    } catch (err) {
-        console.error("MongoDB Connection Error: ", err);
-    }
 }
 
 module.exports = connectDatabase;
