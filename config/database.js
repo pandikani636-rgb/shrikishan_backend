8f816f68b0a7a7a41cb70cb69a8e3187ce6bbde6
pandikani <pandikani636@gmail.com>
2026-08-19 16:07:15 +0530

    Debug DB connection

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/flipkart";
 
 
 const connectDatabase = () => {
+    console.log("Connecting to MongoDB URI starting with:", MONGO_URI.substring(0, 20));
     mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log("Mongoose Connected");
